785d50b15633b474115451963c10c3cd4246c92f
Ben Moskovitz <ben@mosk.nz>
2016-05-15 17:25:44 +1200

    Initial commit

---
+++ rename.rb
@@ -0,0 +1,13 @@
+require 'httparty'
+require 'awesome_print'
+require 'json'
+
+TV_MAZE = "http://api.tvmaze.com/"
+
+puts "What's the show you're looking for?"
+show_name = gets
+shows = HTTParty.get(TV_MAZE + "search/shows?q=" + show_name.gsub(" ", "-"))
+
+shows.each do |show|
+  puts show[:title]
+end
